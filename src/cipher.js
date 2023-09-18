b68cfd7c95f3e9064f832187ec4f1f930539b619
Tharla <jaroxeski2021@gmail.com>
2023-09-18 15:49:14 -0300

    index.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,92 @@
 const cipher = {
-  // ...
+  encode,
+  decode
 };
 
 export default cipher;
+
+function encode(offset, string) {
+  try {
+    if (
+      offset === "" ||
+      string === "" ||
+      typeof string !== "string" ||
+      offset === null ||
+      offset === "0" ||
+      isNaN(offset) ||
+      parseInt(offset) < 1 ||
+      parseInt(offset) > 99
+    ) {
+      throw new TypeError(
+        "Por favor, preencha todos os campos obrigatórios de acordo com as regras de funcionalidades."
+      );
+    }
+
+    const chave = parseInt(offset);
+    const tamanhoAlfabeto = 26;
+
+    let senhaEncriptada = "";
+
+    for (let i = 0; i < string.length; i++) {
+      const char = string[i];
+
+      if (char.match(/[a-zA-Z]/)) {
+        const isUpperCase = char === char.toUpperCase();
+        const primeiraLetra = isUpperCase ? "A".charCodeAt(0) : "a".charCodeAt(0);
+
+        const tabela = (char.charCodeAt(0) - primeiraLetra + chave) % tamanhoAlfabeto + primeiraLetra;
+        const letra = String.fromCharCode(tabela);
+        senhaEncriptada += letra;
+      } else {
+        senhaEncriptada += char;
+      }
+    }
+
+    return senhaEncriptada;
+  } catch (e) {
+    return e.message;
+  }
+}
+
+function decode(offset, string) {
+  try {
+    if (
+      offset === "" ||
+      string === "" ||
+      typeof string !== "string" ||
+      offset === null ||
+      offset === "0" ||
+      isNaN(offset) ||
+      parseInt(offset) < 1 ||
+      parseInt(offset) > 99
+    ) {
+      throw new TypeError(
+        "Por favor, preencha todos os campos obrigatórios de acordo com as regras de funcionalidades."
+      );
+    }
+
+    const chave = parseInt(offset);
+    const tamanhoAlfabeto = 26;
+
+    let senhaDecriptada = "";
+
+    for (let i = 0; i < string.length; i++) {
+      const char = string[i];
+
+      if (char.match(/[a-zA-Z]/)) {
+        const isUpperCase = char === char.toUpperCase();
+        const primeiraLetra = isUpperCase ? "A".charCodeAt(0) : "a".charCodeAt(0);
+
+        const tabela = (char.charCodeAt(0) - primeiraLetra - chave + tamanhoAlfabeto) % tamanhoAlfabeto + primeiraLetra;
+        const letra = String.fromCharCode(tabela);
+        senhaDecriptada += letra;
+      } else {
+        senhaDecriptada += char;
+      }
+    }
+
+    return senhaDecriptada;
+  } catch (e) {
+    return e.message;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
-import cipher from './cipher.js';
+import cipher from "./cipher";
+// Função para codificar um texto usando a cifra de César
+function codificarCifra(texto, deslocamento) {
+  // Variável para armazenar o resultado codificado
+  let resultado = "";
+  
+  // Loop para percorrer cada caractere do texto
+  for (let i = 0; i < texto.length; i++) {
+    // Captura o caractere atual
+    const letra = texto[i];
+  
+    // Verifica se o caractere é uma letra maiúscula
+    if (letra >= "A" && letra <= "Z") {
+      // Calcula o código ASCII da letra atual em relação à letra "A"
+      const codigoLetra = letra.charCodeAt(0) - "A".charCodeAt(0);
+  
+      // Aplica o deslocamento e assegura que o resultado permanece no alfabeto (26 letras)
+      const codigoCodificado = (codigoLetra + deslocamento) % 26;
+  
+      // Converte o código codificado de volta para um caractere e concatena ao resultado
+      const letraCodificada = String.fromCharCode(codigoCodificado + "A".charCodeAt(0));
+      resultado += letraCodificada;
+    }
+    // Verifica se o caractere é uma letra minúscula
+    else if (letra >= "a" && letra <= "z") {
+      // Calcula o código ASCII da letra atual em relação à letra "a"
+      const codigoLetra = letra.charCodeAt(0) - "a".charCodeAt(0);
+  
+      // Aplica o deslocamento e assegura que o resultado permanece no alfabeto (26 letras)
+      const codigoCodificado = (codigoLetra + deslocamento) % 26;
+  
+      // Converte o código codificado de volta para um caractere e concatena ao resultado
+      const letraCodificada = String.fromCharCode(codigoCodificado + "a".charCodeAt(0));
+      resultado += letraCodificada;
+    }
+    // Se o caractere não for uma letra, mantenha-o inalterado
+    else {
+      resultado += letra;
+    }
+  }
+  
+  // Retorna o resultado final
+  return resultado;
+}
+  
 
-console.log(cipher);
+  
